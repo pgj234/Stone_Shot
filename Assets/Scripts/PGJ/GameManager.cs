2381dcc4ac0b288e150fc1895f2c58ae3663fdb8
pgj234 <pgj94@naver.com>
2025-09-07 17:05:07 +0900

    PGJ
    게임매니저에 턴 시스템 틀 추가

---
+++ Assets/Scripts/PGJ/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static public GameManager instance;
+
+    void Awake()
+    {
+        if (null == instance)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    bool isPlayerTurn = true;
+
+    void Start()
+    {
+        TurnInit();
+    }
+
+    void TurnInit()
+    {
+        isPlayerTurn = true;
+    }
+
+    /// <summary>
+    /// 턴 바꾸기
+    /// </summary>
+    internal void TurnChange()
+    {
+        isPlayerTurn = !isPlayerTurn;
+    }
+
+    /// <summary>
+    /// 플레이어 턴인지 체크
+    /// </summary>
+    internal bool PlayerTurnCheck()
+    {
+        return isPlayerTurn;
+    }
+}
